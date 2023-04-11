f60c97c9debfe9f48888f4c3a4bdb884828ee17a
Kamilya <kami130531@gmail.com>
2023-04-11 23:12:22 +0800

    HOMEWORK5

--- HW5/task2.java
+++ HW5/task2.java
@@ -42,7 +42,11 @@ public class task2 {
         }
     }
     public static void main(String[] args) {
-        String[] emploees = new String[] {"Иван Иванов", "Иван Петров", "Сергей Козлов", "Евгений Петров", "Сергей Васильев", "Иван Смирнов", "Андрей Петров"};
+        String[] emploees = new String[] {"Иван Иванов", "Светлана Петрова", "Кристина Белова",
+        "Анна Мусина", "Анна Крутова", "Иван Юрин", "Петр Лыков",
+        "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова",
+        "Мария Савина", "Мария Рыкова", "Марина Лугова", "Анна Владимирова",
+        "Иван Мечников", "Петр Петин", "Иван Ежов"};
         ArrayList<String> emploeesName = getName(emploees);
         Map<String, Integer> mapName =  getMap(emploeesName);
         System.out.println("Повторные имена: ");
